4420556a56b2460c485337192fcbc21213df17bf
SalmonSoara <stsuyemoto@gmail.com>
2025-10-20 00:02:17 -0700

    Inputs

---
+++ SSuyemoto_Hour7_P2/Assets/Scripts/quiz.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quiz : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 10; i > 0; i=i-2)
+        {
+            Debug.Log(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SSuyemoto_Hour8_P1/Assets/scripts/SideQuest.cs
@@ -0,0 +1,6 @@
+/*
+1. The method's name is Takedamage.
+2. The method returns the variable type int.
+3. The method has one parameter called damageAmount.
+4. The code in the method's block is: int = 100; return health - damageAmount.
+*/
