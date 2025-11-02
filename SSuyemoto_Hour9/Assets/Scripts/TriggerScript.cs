2d05e250b301d2a3cbd9f42b3f2e3751841acb5d
SalmonSoara <stsuyemoto@gmail.com>
2025-11-01 23:43:19 -0700

    Collision/physics materials

---
+++ SSuyemoto_Hour9/Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject.name + " has entered the " + this.gameObject.name/*gameObject.ToString()*/ /*name*/); //none of these work to say "RTrigger/LTrigger"... ?
+    }
+    void OnTriggerStay(Collider other)
+    {
+        print(other.gameObject.name + " is still in the " + this.gameObject.name);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        print(other.gameObject.name + " has left the " + this.gameObject.name); 
+    }
+}
