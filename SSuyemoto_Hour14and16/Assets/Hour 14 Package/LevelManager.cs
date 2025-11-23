f8b69fdb6527a03326f48c51dfd7ff4a03e658a8
SalmonSoara <stsuyemoto@gmail.com>
2025-11-22 23:31:08 -0800

    ui push

---
+++ SSuyemoto_Hour14and16/Assets/Hour 14 Package/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public string sceneToLoad = "Game";
+    public string sceneToInstruct = "Instructions";
+    public string sceneToMenu = "ssuyemoto_Hour14";
+
+    public void LoadGame ()
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+
+    public void LoadInstructions()
+    {
+        SceneManager.LoadScene(sceneToInstruct);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(sceneToMenu);
+    }
+}
---
+++ SSuyemoto_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,9 @@
+/* 
+ * -Doodled an illustration for the background of the menus
+ * -Imported a free font, changed the colors/styles to aim for a silly but intense look
+ * -Changed the Start button to change it's text to "Placeholder" when pressed
+ * -Duplicated the Start button to make a placeholder Settings button
+ * -Created a How to Play button, then edited the level manager script to lead to a How to Play page
+ * -Created a simple instructional page for the game; created a button to return to the main menu
+ 
+ */
