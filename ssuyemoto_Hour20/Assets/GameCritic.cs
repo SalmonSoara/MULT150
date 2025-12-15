57850a0a33f2fa342fa04a48560647efcf838797
SalmonSoara <stsuyemoto@gmail.com>
2025-12-15 01:01:21 -0800

    Gauntlet Runner

---
+++ ssuyemoto_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,34 @@
+/*
+1. Is the game too easy or hard?
+    
+    I think it's a fair amount of difficulty at first, the rules are definitely easy to understand and it's easy to figure out a possible strategy to get farther, but the way to execute can be challenging.
+
+2. What changes would you do to make it easier or harder?
+
+    I think I'd adjust the traction of the character's movement to make it more responsive and maybe easier to dodge. But also in turn, have the speed of the obstacles speed up every time a 
+    powerup is collected to balance it to be more challenging again.
+
+3. What would make the game more appealing?
+    
+    Sound effects for collisions, maybe an exciting background song, and more visuals effects with collisions.
+
+4. What parts of the game are fun? 
+
+    I think the simple rules are a good hook, the displayed time increasing/decreasing is definitely an encouragement or pressure to get a higher score, and the spacing of the obstacles makes it exciting to dodge at first.
+
+5. What parts of the game are tedious?
+
+    The delayed direction change doesn't feel the best; it's a little difficult to tell if you hit something sometimes; and once you get the hang of the technique to dodge, the speed of the gameplay isn't challenging anymore.
+
+Changes to Obstacle:
+    -Shape changed to circular spiky blade
+    -Material changed to look metallic
+    -Given a higher intensity red point light
+
+Changes to Powerup:
+    -Changed shape to a lightning bolt icon
+    -Changed effect to a popping type of glowing
+    -Material changed to reflective green/blue
+    -changed point light color to blue
+
+ */
