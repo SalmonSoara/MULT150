24de460fcb1dec03a23a63ab01dfcf939e215947
SalmonSoara <stsuyemoto@gmail.com>
2025-12-19 19:23:40 -0800

    Gauntlet Runner 2

---
+++ ssuyemoto_Final/Assets/Modifications.cs
@@ -0,0 +1,59 @@
+﻿public class Modifications
+{/*
+    ░▒▓████████▓▒░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ */
+
+
+    // Choose 1 of the games below and list modifications 
+    // Delete the other 2 games
+
+    /*  ================================================================
+        GAUNTLET RUNNER
+        ================================================================
+        1 - directional light (colour, intensity, etc)
+        modifications: Color changed to orange, intensity: .8, rotation 50, -30, 0.
+
+        2 - fog
+        modifications: green color, density: .04, exponential squared mode.
+
+        3 - create & apply skybox and customise it (atmosphere thickness, sky tint, exposure, etc)
+        modifications: atmosphere thickness: .17, exposure: 1.1, sky tint: green, simple sun, sun size 17.
+        
+        OBSTACLE PREFAB
+        4 - add or modify light component (colour, range, intensity, etc)
+        modifications: range: 2.5, intensity 3, kept the same red.
+        
+        POWERUP PREFAB
+        5 - replace with a new complex object comprised of 3 or more shapes (different from previously made object)
+        modifications: Created a battery-like shape 
+
+        6 - use gameobject hierarchy to keep the powerup prefab organised
+        [no record necessary]
+
+        7 - apply at least 2 new textures
+        (add source url or package name)
+        texture source 1: black_diamonds.jpg
+        texture source 2: green_fabric.jpg 
+                 (photos I took)
+
+        8 - add particle system component and customise at least 3 parameters
+        parameter 1: rotation by speed, angular velocity: 60
+        parameter 2: Ribbon trails, 1 count, inherit particle color
+        parameter 3: green start color
+
+        WALL
+        9 - convert a wall to prefab, create a variant, and use the variant to replace the other wall in the scene
+        [no record necessary]
+
+        10 - modify wall prefab material tint so that the 2 walls are different in appearance or colour
+        [no record necessary]
+     
+     */
+
+
+}
